a46888c38f00afc7c545c1fa753d51d2828d64f9
sripriyavytla <sri.priya@celigo.com>
2024-07-08 22:16:54 +0530

    all files

--- src/Ex_07062024/Lab001.java
+++ src/Ex_07062024/Lab001.java
@@ -1,2 +1,7 @@
-package Ex_07062024;public class Lab001 {
+package Ex_07062024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
